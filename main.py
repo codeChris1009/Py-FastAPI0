62ee2fd4949378ad27f9f948fb0fa6339309b3a2
Alan Su <susx@AlandeMacBook-Air.local>
2025-08-14 15:01:18 +0800

    [Modify]get post for bookshelf and mockdata

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+from typing import List,  Optional
+from uuid import UUID, uuid4
 from datetime import date
 
 app = FastAPI()
 
 
+class Reflection(BaseModel):
+    rate_star: Optional[int] = 1
+    desc: Optional[str] = None
+
+class Book(BaseModel):
+    id: Optional[UUID] = None
+    isbn: str
+    author: Optional[str] = None
+    title: Optional[str] = None
+    lang: str
+    isRead: bool = False
+    reflection: Optional[Reflection] = None
+
+
+bookshelf = []
+
+# Welcome Message
 @app.get("/")
-def read_root():
+def welcome_message():
     today = date.today()
     week = today.strftime("%A")
     return {"welcomeMessage": f"{week}, Happy Book, Code, Cookie With Padi~"}
 
+
+# Create Post
+@app.post("/bookshelf", response_model=Book)
+def create_book(book: Book):
+    book.id = uuid4()
+    bookshelf.append(book)
+    return book
+
+# Read Books
+@app.get("/bookshelf", response_model=List[Book])
+def read_books():
+    return bookshelf
 
 if __name__ == "__main__":
     import uvicorn

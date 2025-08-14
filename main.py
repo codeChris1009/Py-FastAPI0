d03ef5e693dcb68ab78706bad27f5f380149aca5
Alan Su <susx@AlandeMacBook-Air.local>
2025-08-14 15:25:18 +0800

    [Modify] Advenced Get Done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 from pydantic import BaseModel
 
 from typing import List,  Optional
@@ -40,9 +40,37 @@ def create_book(book: Book):
     return book
 
 # Read Books
+
+# Get all bookshelf
 @app.get("/bookshelf", response_model=List[Book])
 def read_books():
     return bookshelf
+
+# Get by ISBN
+@app.get("/bookshelf/{isbn}", response_model=Book)
+def read_bookByISBN(isbn: str):
+    for book in bookshelf:
+        if book.isbn == isbn:
+            return book
+    raise HTTPException(status_code=404, detail=f"Book not found, please check the ISBN：{isbn}")
+
+
+
+# Get by Title
+@app.get("/bookshelf/title/{title}", response_model=List[Book])
+def read_booksByTitle(title: str):
+    matched_books = [book for book in bookshelf if book.title == title]
+    if matched_books:
+        return matched_books
+    raise HTTPException(status_code=404, detail=f"Book not found, please check the Title：{title}")
+
+# Get by Author
+@app.get("/bookshelf/author/{author}", response_model=List[Book])
+def read_booksByAuthor(author: str):
+    matched_books = [book for book in bookshelf if book.author == author]
+    if matched_books:
+        return matched_books
+    raise HTTPException(status_code=404, detail=f"Book not found, please check the Author：{author}")
 
 if __name__ == "__main__":
     import uvicorn
